27bcc59ade0bdc3d3184662b6f8f9480d5a440cd
Ayan Maji <88241719+theayan44@users.noreply.github.com>
2023-10-26 21:38:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+	this.name = name;
+	this.age = age;
+	this.jobTitle = jobTitle;
+}
+
+Person.prototype.greet = function(){
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old`);
+}
+
+Employee.prototype.jobGreet = function(){
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`);
+}
+
+Employee.prototype.__proto__ = Person;
 
 // Do not change code below this line
 window.Person = Person;
